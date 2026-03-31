90d1a3c3256b4b2fafb511a42876df585b9d95c3
Loïc Gnocchini <loic.gnocchini@umontreal.ca>
2026-03-31 12:20:35 -0400

    added energy, hamming

--- src/hopfield/analysis/energy.py
+++ src/hopfield/analysis/energy.py
@@ -1,9 +1,15 @@
 import numpy as np
 from numba import njit
+from numpy.typing import NDArray
 
 """
 energy function
 """
 
-def calculate_energy(w):
-    return
+def calculate_energy(state: NDArray[np.int8], 
+                     weight_matrix: NDArray[np.float64]
+                     ) -> float:
+
+    return float(- 0.5 * state @ weight_matrix @ state)
+
+    
--- src/hopfield/analysis/hamming.py
+++ src/hopfield/analysis/hamming.py
@@ -1,9 +1,22 @@
 import numpy as np
 from numba import njit
+from numpy.typing import NDArray
+
 
 """
 hamming distance function
 """
 
-def calculate_hamming():
-    return
+def calculate_hamming(state: NDArray[np.int8], 
+                      pattern: NDArray[np.int8]
+                      ) -> float:
+    
+    return int((state.size - np.dot(pattern,state)) / 2)
+
+    
+
+def normalized_hamming(state: NDArray[np.int8], 
+                      pattern: NDArray[np.int8]
+                      ) -> float:
+    
+    return float((state.size - np.dot(pattern,state)) / (2*state.size))
